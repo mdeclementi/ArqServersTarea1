668eae1e84b12cafd243954109126ba02080be2c
mdeclementi <marcusdeclementi@gmail.com>
2023-10-01 03:16:15 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors'); app.use(cors());
 const EmployeesRoutes = require('./Routers/Employees');
 
 app.use(express.json());
 
-mongoose
-  .connect(
-    "mongodb+srv://api:Mgv3DsML0k0WEUZf@cluster0.w1wifjo.mongodb.net/tareaBACKEND?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("Connected to database!");
-  })
-  .catch(() => {
-    console.log("Connection failed!");
-  });
-
-//SE INICIA LA APLICACION EN PUERTO 3000
-app.listen(3000, () => {
+app.listen(8000, () => {
     console.log('server is running')
 })
 
